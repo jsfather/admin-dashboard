1a9258a2c48c2a6bd67572d46e5b3fd57a6f42ab
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 19:25:22 +0330

    shadow added

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <div class="bg-base shadow-sm h-24 px-6 flex flex-row">
+  <div class="bg-base h-24 px-6 flex flex-row">
     <app-input placeholder="Search something..." prependIcon>
       <template #prependIcon>
         <svg-icons-magnify class="text-opposite h-4 w-4"  />
