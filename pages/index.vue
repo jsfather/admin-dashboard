432cb938235120e07f3750f6ac83f244836e5db8
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 11:21:39 +0330

    auto-animate added

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,27 @@
 <script setup lang="ts">
+import { useAutoAnimate } from '@formkit/auto-animate/vue'
+
+const items = ref(["😏","😐","😑","😒","😕" ])
+function removeItem(toRemove: any) {
+  items.value = items.value.filter((item) => item !== toRemove)
+}
+
+const [parent] = useAutoAnimate()
 
 </script>
 
 <template>
   <h1 class="text-primary">Index Page</h1>
+  <h5>Click emojis to remove them.</h5>
+  <ul ref="parent" class="flex flex-row">
+    <li
+        v-for="item in items"
+        :key="item"
+        @click="removeItem(item)"
+    >
+      {{ item }}
+    </li>
+  </ul>
 </template>
 
 <style scoped>
