aa68e5305c8988062bf5e7654784034d4259c17c
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 19:26:35 +0330

    @nuxt/icon added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,9 @@
 export default defineNuxtConfig({
     compatibilityDate: '2024-04-03',
-    modules: ["@nuxtjs/tailwindcss", "@formkit/auto-animate/nuxt", "@vueuse/nuxt"]
+    modules: [
+        "@nuxtjs/tailwindcss",
+        "@formkit/auto-animate/nuxt",
+        "@vueuse/nuxt",
+        "@nuxt/icon"
+    ]
 })
