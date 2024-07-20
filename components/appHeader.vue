1017aba3a0ad5ee576fa746e1a83875739336e64
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 16:33:17 +0330

    search input added

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -3,7 +3,13 @@
 </script>
 
 <template>
-
+  <div class="bg-base shadow-sm h-24 px-6 flex flex-row">
+    <app-input placeholder="Search something..." prependIcon>
+      <template #prependIcon>
+        <svg-icons-magnify class="text-opposite h-4 w-4"  />
+      </template>
+    </app-input>
+  </div>
 </template>
 
 <style scoped>
