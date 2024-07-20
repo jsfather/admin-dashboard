9c83a1418052e664fa9da0c693b484966b30c90f
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 19:25:40 +0330

    nuxt-icons added

--- components/appHeader.vue
+++ components/appHeader.vue
@@ -6,7 +6,7 @@
   <div class="bg-base h-24 px-6 flex flex-row">
     <app-input placeholder="Search something..." prependIcon>
       <template #prependIcon>
-        <svg-icons-magnify class="text-opposite h-4 w-4"  />
+        <Icon name="solar:magnifer-linear" class="text-opposite" />
       </template>
     </app-input>
   </div>
