5068852e4d1628eb0ca1c2b7f998fbdebc26e0f6
jsfather <keyvanmatinrad@outlook.com>
2024-07-20 19:25:53 +0330

    appDrawer.vue added

---
+++ components/appDrawer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+const items = [{title: 'About'}, {title: 'Services'}, {title: 'Clients'}, {title: 'Contact'}]
+</script>
+
+<template>
+  <div class="w-64 fixed top-0 h-full bg-base p-2 shadow-lg">
+    <a v-for="item in items" href="#" class="block rounded-2xl hover:text-primary hover:bg-background py-3 px-4">{{
+        item.title
+      }}</a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
